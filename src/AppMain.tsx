efdfcc250e45ce585f731bbf4af5791879b7359a
minwoo129 <key0129mw@naver.com>
2024-01-30 13:05:57 +0900

    앱 접속시 기존 기록 초기화 처리

--- src/AppMain.tsx
+++ src/AppMain.tsx
@@ -1,10 +1,37 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {AppMainProps} from './types';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStackNavigatior from './component/navigation/RootStackNavigatior';
+import {useAppDispatch} from './hooks';
+import {getStorageData} from './storage';
+import {QuizRecordData} from './storage/types';
+import {setQuizRecords} from './redux/slice/quiz';
 
 const AppMain: FC<AppMainProps> = ({}) => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    initialize();
+  }, []);
+
+  const initialize = async () => {
+    try {
+      const quizRecords = await getQuizRecords();
+      if (quizRecords) {
+        dispatch(setQuizRecords(quizRecords.records));
+      }
+    } catch (e) {}
+  };
+
+  const getQuizRecords = async () => {
+    try {
+      const result = getStorageData<QuizRecordData>('quizRecord');
+      return result;
+    } catch (error) {
+      return null;
+    }
+  };
   return (
     <>
       <NavigationContainer>
