47e7df31faee835174b26093d03cf3f6503e9cac
minwoo129 <key0129mw@naver.com>
2024-02-01 13:28:12 +0900

    CommonButton 테스트 케이스 코드 추가

--- __tests__/CommonButton.test.tsx
+++ __tests__/CommonButton.test.tsx
@@ -4,7 +4,18 @@ import CommonButton from '../src/component/common/CommonButton';
 
 import {it} from '@jest/globals';
 import renderer from 'react-test-renderer';
+import {Text} from 'react-native';
 
-it('renders correctly', () => {
-  renderer.create(<CommonButton title="" />);
+describe('CommonButton', () => {
+  it('렌더링이 정상적으로 이루어지는가?', () => {
+    renderer.create(<CommonButton title="" />);
+  });
+
+  it('임의로 입력한 title에 따라서 버튼의 title이 변경되는가?', () => {
+    const testTitle = 'Test Title';
+    const tree = renderer.create(<CommonButton title={testTitle} />).root;
+
+    const textComponent = tree.findByType(Text);
+    expect(textComponent.props.children).toBe(testTitle);
+  });
 });
