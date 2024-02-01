4b631b63dbd5f1ca163c5a0730a9e9e2861101bb
minwoo129 <key0129mw@naver.com>
2024-02-01 14:32:21 +0900

    dayjs 확장 프로그램 연결
    - LocalizedFormat

--- index.js
+++ index.js
@@ -5,5 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import dayjs from 'dayjs';
+import LocalizedFormat from 'dayjs/plugin/localizedFormat';
+
+dayjs.extend(LocalizedFormat);
 
 AppRegistry.registerComponent(appName, () => App);
