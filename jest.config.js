9746b16d86d41dd3ef9dcef304166eb6eadb3f98
minwoo129 <key0129mw@naver.com>
2024-02-01 13:07:23 +0900

    jest 작업환경 설정

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['./jest/setup.js'],
+  moduleNameMapper: {
+    '/.(jpg|jpeg|png|gif|webp|svg)$': './__mocks__/fileMock.js',
+  },
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
 };
---
+++ jest/setup.js
@@ -0,0 +1,7 @@
+import 'react-native-gesture-handler/jestSetup';
+
+jest.mock('@react-native-async-storage/async-storage', () => ({
+  setItem: jest.fn(() => Promise.resolve(null)),
+  getItem: jest.fn(() => Promise.resolve(null)),
+  removeItem: jest.fn(() => Promise.resolve(null)),
+}));
